876e940aaa7602fd354788a403ba47b6745fc3a2
Jan Hrubes <heinzz@chatalansko.cz>
2021-12-13 20:06:14 +0000

    copy over .ssh keys for development

--- install_arch_secure.sh
+++ install_arch_secure.sh
@@ -284,8 +284,10 @@ echo "vm.swappiness=10" > /mnt/etc/sysctl.d/99-swappiness.conf
 
 ## DEBUG - copy over the install scripts to be able to work on them in the OS
 cp -r /root/Arch_install /mnt/home/${USERNAME}/
+cp -r /root/.ssh /mnt/home/${USERNAME}/
 UID=$(grep ${USERNAME} /mnt/etc/passwd | cut -d ':' -t 4 )
 chown -R ${UID}:${UID} /mnt/home/${USERNAME}/Arch_install
+chown -R ${UID}:${UID} /mnt/home/${USERNAME}/.ssh
 
 ## before reboot, make sure to remove old passphrase from cryptroot if using FIDO2 token.
 ## not yet, debuugign and token doesnt work in arch live iso... 
