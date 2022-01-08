d33a8ff33c3a6bf38253178f3050d2364943a649
Jan Hrubes <jan.hrubes@nic.cz>
2022-01-08 17:42:25 +0100

    Change test script to source files properly

--- test.sh
+++ test.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-source vars.sh
-source common_functions.sh
+source ${SCRIPT_DIR}/vars.sh
+source ${SCRIPT_DIR}/common_functions.sh
 
 trap exit 1 ERR
 trap exit 1 SIGINT
+
+CRYPT_PARTITION=${INSTALL_PARTITION}p2
+BOOT_PARTITION=${INSTALL_PARTITION}p1
+
+notify ${INSTALL_PARTITION}
+notify ${CRYPT_PARTITION}
+notify ${BOOT_PARTITION}
+
+INSTALL_PARTITION="/dev/"$(get_valid_input "lsblk -d" "block device to install")
+
+CRYPT_PARTITION=${INSTALL_PARTITION}p2
+BOOT_PARTITION=${INSTALL_PARTITION}p1
+
+notify ${INSTALL_PARTITION}
+notify ${CRYPT_PARTITION}
+notify ${BOOT_PARTITION}
 
 notify "all is well"
