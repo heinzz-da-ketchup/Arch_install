4b52f0e9c2058f5f9309f660767601ce65bf7883
Jan Hrubes <jan.hrubes@nic.cz>
2022-01-08 16:14:49 +0100

    for some reason -q flag in grep breaks things.. (-_-)

--- common_functions.sh
+++ common_functions.sh
@@ -71,11 +71,11 @@ get_valid_input (){
 	printf ${GREEN} >/dev/tty
 	printf "\n##############################\n" >/dev/tty
 
-	while ! [[ $(grep -qw ${Input} <<< ${Prompt}) ]]; do
+	while ! [[ $(grep -w ${Input} <<< ${Prompt}) ]]; do
 		printf ${GREEN}"Please set ${2}\n"${NC} >/dev/tty
 		read Input
 
-		if ! [[ $(grep -qw ${Input} <<< ${Prompt}) ]]; then
+		if ! [[ $(grep -w ${Input} <<< ${Prompt}) ]]; then
 		    printf ${RED}"Wrong ${2}\n\n"${NC} >/dev/tty
 		fi
 	done
