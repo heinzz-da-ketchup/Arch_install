449c46cc0197e4e30f0779c7241c6146d3a30223
Jan Hrubes <jan.hrubes@nic.cz>
2022-01-19 00:17:38 +0100

    fix include/source in test.sh, delete testing shit

--- test.sh
+++ test.sh
@@ -2,26 +2,10 @@
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-source ${SCRIPT_DIR}/vars.sh
 source ${SCRIPT_DIR}/common_functions.sh
+source ${SCRIPT_DIR}/vars.sh
 
 trap exit 1 ERR
 trap exit 1 SIGINT
-
-CRYPT_PARTITION=${INSTALL_PARTITION}p2
-BOOT_PARTITION=${INSTALL_PARTITION}p1
-
-notify ${INSTALL_PARTITION}
-notify ${CRYPT_PARTITION}
-notify ${BOOT_PARTITION}
-
-INSTALL_PARTITION="/dev/"$(get_valid_input "lsblk -d" "block device to install")
-
-CRYPT_PARTITION=${INSTALL_PARTITION}p2
-BOOT_PARTITION=${INSTALL_PARTITION}p1
-
-notify ${INSTALL_PARTITION}
-notify ${CRYPT_PARTITION}
-notify ${BOOT_PARTITION}
 
 notify "all is well"
