587c8abf731047d87b6a9417fc69bda63c277abb
Jan Hrubes <jan.hrubes@nic.cz>
2022-01-23 22:02:22 +0100

    Cleanup and some better logic for vars.sh

--- vars.sh
+++ vars.sh
@@ -21,7 +21,7 @@ ONLY_MOUNT=false				## DEBUG - Stop script after mounting filesystems
 CHROOT_PREFIX="arch-chroot /mnt"
 KEYMAP="cz-qwertz" 				
 
-USERSW="networkmanager vim git openssh cifs-utils"
+USERSW="networkmanager vim git openssh"
 BASICUTILS="btrfs-progs man-db man-pages texinfo grub efibootmgr sudo sbsigntools polkit"
 
 ## Script will ask or use defaults if empty
@@ -38,40 +38,36 @@ SAMBA_USER=""			## If $SAMBA_SHARES are not empty, script will ask for username 
 SAMBA_PW=""
 
 ## ----------------------------------------------
-## Prepare rest of variables, set some sane defaults if needed
+## Prepare rest of variables, set some sane defaults if needed - only if we are running the install script
 ## ----------------------------------------------
 
-## Ask for user input if we need to
-if [[ -z ${INSTALL_PARTITION} ]]; then
-    INSTALL_PARTITION="/dev/"$(get_valid_input "lsblk -d" "block device to install")   
-fi
+if [[ $0 =~ "install_arch_secure.sh" ]]; then
+    ## Ask for user input if we need to
+    if [[ -z ${INSTALL_PARTITION} ]]; then
+	INSTALL_PARTITION="/dev/"$(get_valid_input "lsblk -d" "block device to install")   
+    fi
 
-if [[ -z ${USERNAME} ]]; then 
-    USERNAME=$(get_confirmed_input "username") 
-fi
+    if [[ -z ${USERNAME} ]]; then 
+	USERNAME=$(get_confirmed_input "username") 
+    fi
 
-if [[ -z ${HOSTNAME} ]]; then 
-    HOSTNAME=$(get_confirmed_input "hostname")
-fi
+    if [[ -z ${HOSTNAME} ]]; then 
+	HOSTNAME=$(get_confirmed_input "hostname")
+    fi
 
-if [[ -n ${SAMBA_SHARES} ]]; then
-    [[ -z ${SAMBA_USER} ]] && SAMBA_USER=$(get_confirmed_input "Samba user")
-    [[ -z ${SAMBA_PW} ]] && SAMBA_PW=$(get_confirmed_input "Password for samba user ${SAMBA_USER}")
+    if [[ -n ${SAMBA_SHARES} ]]; then
+	[[ -z ${SAMBA_USER} ]] && SAMBA_USER=$(get_confirmed_input "Samba user")
+	[[ -z ${SAMBA_PW} ]] && SAMBA_PW=$(get_confirmed_input "Password for samba user ${SAMBA_USER}")
+    fi
 fi
 
 ## For others, we have sane defaults
 if [[ -z ${BUILDDIR} ]]; then
 	BUILDDIR="/mnt/home/${USERNAME}/builds"
-	BUILDDIR_CHROOT="/home/${USERNAME}/builds"
-else
-	BUILDDIR_CHROOT=$(sed 's|/mnt||' <<< ${BUILDDIR})
 fi
 
 if [[ -z ${MOKDIR} ]]; then
 	MOKDIR="/mnt/home/${USERNAME}/.mok"
-	MOKDIR_CHROOT="/home/${USERNAME}/.mok"
-else
-	MOKDIR_CHROOT=$(sed 's|/mnt||' <<< ${MOKDIR})
 fi
 
 if [[ -z ${SWAPFILE} ]]; then
@@ -83,7 +79,14 @@ if ! [[ ${FIDO2_DISABLE} == ture ]]; then
     BASICUTILS="${BASICUTILS} libfido2 pam-u2f"
 fi
 
+## Add needed packages if we are using samba shares
+if [[ -n ${SAMBA_SHARES} ]]; then
+    BASICUTILS="${BASICUTILS} cifs-utils"
+fi
+
 CRYPT_PARTITION=${INSTALL_PARTITION}p2
 BOOT_PARTITION=${INSTALL_PARTITION}p1
 SWAPDIR=$(grep -o '.*/'<<< ${SWAPFILE})
 INSTALLSW="${USERSW} ${BASICUTILS}"
+BUILDDIR_CHROOT=$(sed 's|/mnt||' <<< ${BUILDDIR})
+MOKDIR_CHROOT=$(sed 's|/mnt||' <<< ${MOKDIR})
