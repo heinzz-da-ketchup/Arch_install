92529f2afb1cf5b8d75a63f9ff4b8e7e7978b6c0
Jan Hrubes <jan.hrubes@nic.cz>
2022-05-18 19:46:53 +0200

    config_archiso.sh: source functions first

--- config_archiso.sh
+++ config_archiso.sh
@@ -2,8 +2,8 @@
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-source ${SCRIPT_DIR}/vars.sh
 source ${SCRIPT_DIR}/common_functions.sh
+source ${SCRIPT_DIR}/vars.sh
 
 ## Prepare packages and binary files to use
 notify "Installing 'archiso'"
